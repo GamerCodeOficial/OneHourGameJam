82164c8e3166dda90b6a9b670731ccdb208098e9
Marcelo Junqueira <Marcelo Junqueira>
2017-12-23 18:36:40 -0200

    fase 1

--- OneHourGameJam/Assets/Scripts/Menu.cs
+++ OneHourGameJam/Assets/Scripts/Menu.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Menu : MonoBehaviour {
+    public GameObject[] panels;
+    public int lv;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    //first
+    public GameObject msg;
+    public Animator fAnim;
+    public void TryPlay()
+    {
+        msg.SetActive(true);
+    }
+    public void Close() {
+        msg.SetActive(false);
+    }
+    public void Fall() {
+        fAnim.SetBool("Fall",true);
+    }
+//end
+
+
+    // Use this for initialization
+    void Start () {
+        msg.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+   
 }
